87c74f887229b8a6fffeeaaa2e3291267570679b
Ebrahim Mohsen <ibrahimjarb69@gmail.com>
2025-03-13 14:43:08 +0200

    Create auth dto

---
+++ AuraChat/DTOs/AuthDtos.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AuraChat.DTOs;
+
+public class LoginDto
+{
+    [EmailAddress]
+    public string Email { get; set; } = null!;
+    [MinLength(4)]
+    public string Password { get; set; } = null!;
+}
+
+public class ChangePassDto
+{
+    [EmailAddress]
+    public string Email { get; set; } = null!;
+    [MinLength(4)]
+    public string OldPassword { get; set; } = null!;
+    [MinLength(4)]
+    public string NewPassword { get; set; } = null!;
+}
+
+public class RegisterDto
+{
+    [RegularExpression(@"^[\p{L}\s]{2,20}$", ErrorMessage = "only letters and spaces allowed, max length 20 and min 2.")]
+    public string UserName { get; set; } = null!;
+    [EmailAddress]
+    public string Email { get; set; } = null!;
+    [MinLength(4)]
+    public string Password { get; set; } = null!;
+    [MinLength(4)]
+    [Compare(nameof(Password), ErrorMessage = "passwords do not match")]
+    public string ConfirmPassword { get; set; } = null!;
+}
--- AuraChat/Entities/UserSettings.cs
+++ AuraChat/Entities/UserSettings.cs
@@ -5,4 +5,5 @@ public class UserSettings
     public bool IsVisible { get; set; } = true;
     public bool FriendsOnlyMessages { get; set; } = false;
     public bool FriendsOnlyGroupInvite { get; set; } = false;
+    public bool TwoFactorAuthEnabled { get; set; } = true;
 }
